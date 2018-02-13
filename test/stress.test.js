c8138e66cedd83e09e348bcbdd6aaa91db7af2ec
David Babel <david.babel@ogury.co>
2018-02-13 14:21:04 +0100

    udate bench tests timings

--- test/stress.test.js
+++ test/stress.test.js
@@ -2,17 +2,17 @@
 const { generateMinimalVast } = require('./helpers');
 
 describe('Stress test', () => {
-  xit('poc', () => {
-    console.log(generateMinimalVast());
-  });
-  it('should generate 50000 VAST in less than 20s', () => {
+  // it('poc', () => {
+  //   console.log(generateMinimalVast());
+  // });
+  it('should generate 20000 VAST in less than 10s', () => {
     const begin = new Date();
-    for (let i = 0; i < 50000; i++) {
+    for (let i = 0; i < 20000; i++) {
       generateMinimalVast();
     }
     const end = new Date();
 
-    if (end - begin > 20000 ) {
+    if (end - begin > 10000 ) {
       throw new Error('Vast generation was too slow');
     }
   });
